a718b71aa250fef297e0c3028b43c69862626002
millersteventyler <millersteventyler@gmail.com>
2021-06-05 14:17:17 -0400

    Created a create_fleet method. It will make a fleet of 3 robots.

--- fleet.py
+++ fleet.py
@@ -1,3 +1,14 @@
+from robot import Robot
+
+
 class Fleet:
     def __init__(self):
         self.robots = []
+
+    def create_fleet(self):
+        robot1 = Robot('Killer')
+        robot2 = Robot('Slasher')
+        robot3 = Robot('Apple')
+        self.robots.append(robot1)
+        self.robots.append(robot2)
+        self.robots.append(robot3)
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # This is a sample Python script.
 from dinosaur import Dinosaur
-from robot import Robot
+from fleet import Fleet
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    robo1 = Robot('Craig')
+    fleet = Fleet()
+    fleet.create_fleet()
 
 
 print('Game Over')
