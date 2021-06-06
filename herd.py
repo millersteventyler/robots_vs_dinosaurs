32f66f9c4ed95debf7bd638e973ec61d49d1fac4
millersteventyler <millersteventyler@gmail.com>
2021-06-06 15:22:54 -0400

    Created method create_herd. When called it makes a herd of 3 dinosaurs.

--- herd.py
+++ herd.py
@@ -1,3 +1,14 @@
+from dinosaur import Dinosaur
+
+
 class Herd:
     def __init__(self):
         self.dinosaurs = []
+
+    def create_herd(self):
+        dinosaur1 = Dinosaur('T-Rex', 100)
+        dinosaur2 = Dinosaur('velociraptor', 100)
+        dinosaur3 = Dinosaur('Europasaurus', 100)
+        self.dinosaurs.append(dinosaur1)
+        self.dinosaurs.append(dinosaur2)
+        self.dinosaurs.append(dinosaur3)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-from dinosaur import Dinosaur
+from herd import Herd
 from fleet import Fleet
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -8,7 +8,8 @@ from fleet import Fleet
 if __name__ == '__main__':
     fleet = Fleet()
     fleet.create_fleet()
-
+    herd = Herd()
+    herd.create_herd()
 
 print('Game Over')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
